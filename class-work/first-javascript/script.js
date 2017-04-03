610d1b757be47ccbd6d953fcac0f279e68f8971f
aboldero <aboldero@gmail.com>
2017-04-02 22:26:56 -0400

    homework/startup-matchmaker
    - complete
    - footer cheat

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,11 @@
+// var num1 = 10 * 10;
+// var num2 = 100 / 10;
+// var num3 = 50 - 17;
+// var myName = 'Alison Boldero';
+
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(myName);
+
+document.querySelector('#title').style.color= 'red'
