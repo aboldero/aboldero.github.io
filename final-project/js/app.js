94f0c0a7db1e6b70b89ef45059f07568b14a26cf
aboldero <aboldero@gmail.com>
2017-05-10 18:44:26 -0400

    fp/header
    - messing with the header

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -6,14 +6,17 @@ $(window).scroll(function () {
 		if (offset > 50) {
 			$('header').addClass('header-offset');
 			$('h1').hide();
+			$('main').css("margin-top", "125px");
 		}
 		else {
 			$('header').removeClass('header-offset');
 			$('h1').show();
+			$('main').css("margin-top", "250px");
 		}
 	} else {
 		$('header').removeClass('header-offset');
 		$('h1').show();
+		$('main').css("margin-top", "250px");
 	}
 })
 
